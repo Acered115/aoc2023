26d9a88ab7788841e51b9015b7e1491c4efdc324
Acered <acered115@yahoo.com>
2023-12-16 00:43:14 +0000

    Just Made the most disgusting loop based solution to this problem... look at the risk of your own sanity

--- day3/day3_part_one.py
+++ day3/day3_part_one.py
@@ -1,45 +1,118 @@
-line_list = []
-digit_str_list = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+from pprint import pprint
+
+
+def find_symbol_footprint(index, symbol):
+    symbol_object = {}
+
+    # if index==0:
 
 
 def line_parser(line):
-    line_list.append(line)
-    symbol_index = []
-    number_buffer = ""
     number_list = []
-    digit_ends = []
-    # print(line)
+    symbols_list = []
+    number_buffer = ""
+    number_index_buffer = []
     for index, x in enumerate(line):
-        # print(index)
-        if x not in digit_str_list and x != "." and x != "\n":
-            # print(x, index)
-            symbol_index.append((x, index))
-
         if x.isdigit():
-            digit_ends.append(index)
             number_buffer += x
+            number_index_buffer.append(index)
         else:
             if len(number_buffer) != 0:
-                number_list.append({"number": int(number_buffer), "ends": digit_ends})
-                digit_ends = []
-            number_buffer = ""
-    # print(number_list)
+                number = int(number_buffer)
+                number_list.append({"index": number_index_buffer, "number": number})
+                number_buffer = ""
+                number_index_buffer = []
 
-    return {"number_list": number_list, "symbols_index": symbol_index}
+            if x != "." and x != "\n":
+                symbols_list.append({"index": [index], "symbol": x})
+    # print(symbols_list)
+    # print(number_list)
+    return_object = {"numbers": number_list, "symbols": symbols_list}
+    return return_object
+
+
+def symbol_appender():
+    for index, row in enumerate(lines_list):
+        # symbol_appender(index, row)
+        for symbol in row["symbols"]:
+            if index == 0:
+                symbol["index"].append(symbol["index"][-1] + 1)
+                continue
+
+            if index == len(lines_list) - 1:
+                # print(row["symbols"])
+                symbol["index"].insert(0, symbol["index"][0] - 1)
+                continue
+
+            else:
+                symbol["index"].insert(0, symbol["index"][0] - 1)
+                symbol["index"].append(symbol["index"][-1] + 1)
+    # print(row)
+
+
+def check_neighbours():
+    summ = 0
+    for index, row in enumerate(lines_list):
+        print("index:", index)
+        # pprint(lines_list)
+
+        symbol_fields = []
+        numbers_index = []
+        if index == 0:
+            for symbol in lines_list[index]["symbols"]:
+                symbol_fields.extend(symbol["index"])
+            for symbol in lines_list[index + 1]["symbols"]:
+                symbol_fields.extend(symbol["index"])
+
+        elif index == len(lines_list) - 1:
+            for symbol in lines_list[index - 1]["symbols"]:
+                symbol_fields.extend(symbol["index"])
+            for symbol in lines_list[index]["symbols"]:
+                symbol_fields.extend(symbol["index"])
+
+        else:
+            for symbol in lines_list[index - 1]["symbols"]:
+                symbol_fields.extend(symbol["index"])
+            for symbol in lines_list[index]["symbols"]:
+                symbol_fields.extend(symbol["index"])
+            for symbol in lines_list[index + 1]["symbols"]:
+                symbol_fields.extend(symbol["index"])
+
+        for numbers in lines_list[index]["numbers"]:
+            numbers_index.extend(numbers["index"])
+            # print(numbers["index"])
+        print(row["symbols"])
+        print("symbols", symbol_fields)
+        # print("numbers", numbers_index)
+
+        inter_indexes = list(set(symbol_fields) & set(numbers_index))
+        # print(inter_indexes)
+        if len(inter_indexes) >= 1:
+            for numbers in row["numbers"]:
+                # print(numbers["index"])
+                if len(list(set(numbers["index"]) & set(inter_indexes))) > 0:
+                    summ += numbers["number"]
+                # print(numbers)
+
+        # print("\n")
+    print(summ)
+    # print(lines_list[index + 1])
 
 
 # Need to create symbol index checker between adjascent lines
 #
 
 if __name__ == "__main__":
-    sum_of_all = 0
-    print(
-        line_parser(
-            "........................#....374...382....250...*..........737*....*896.395...........*....................$.........................#......\n"
-        )
-    )
-    # with open("puzzle_inputs/test3.txt", "r") as file:
-    #     for line in file:
-    #         line_parser(line)
-    # print((sum_of_all - 114) - 58)
-    # print(line_list)
+    # test_row = line_parser(
+    #     "........................#....374...382....250...*..........737*....*896.395...........*....................$.........................#......\n"
+    # )
+    lines_list = []
+
+    with open("puzzle_inputs/day3.txt", "r") as file:
+        for line in file:
+            current_line = line_parser(line)
+            lines_list.append(current_line)
+    print(current_line)
+    symbol_appender()
+    check_neighbours()
+    # print(lines_list)
