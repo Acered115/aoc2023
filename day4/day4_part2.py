f248e44aaaba5c7599f2dd220bd8019405e855e2
Acered <acered115@yahoo.com>
2023-12-19 17:02:29 +0000

    Day4 part 2 completed, slightly less ugly solution than day 3 lol

--- day4/day4_part2.py
+++ day4/day4_part2.py
@@ -3,11 +3,15 @@ from dataclasses import dataclass
 
 @dataclass
 class Card:
+    """Class used to create Card objects which calculate all the
+    relevant data per card
+    """
+
     line: str
     card_id = 0
-    winners = []
+    win_nums = []
     card_nums = []
-    winning_numbers = []
+    winners = []
     copies = 1
 
     def __post_init__(self):
@@ -18,43 +22,54 @@ class Card:
                 self.card_id = int(string_buffer[4:-1])
                 string_buffer = ""
             if letter == "|":
-                winners = string_buffer[1:-1].split(" ")
-                while "" in winners:
-                    winners.remove("")
+                win_nums = string_buffer[1:-1].split(" ")
+                while "" in win_nums:
+                    win_nums.remove("")
 
-                self.winners = winners
+                self.win_nums = win_nums
                 string_buffer = ""
 
             else:
-                card_num = string_buffer[1:-1].split(" ")
-                while "" in card_num:
-                    card_num.remove("")
+                card_nums = string_buffer[1:-1].split(" ")
+                while "" in card_nums:
+                    card_nums.remove("")
 
-                self.card_nums = card_num
-        self._calc_winners()
-
-    def _calc_winners(self):
-        print(self.winners)
-        print(self.card_nums)
-        self.winning_numbers = list(set(self.winners) & set(self.card_nums))
-        print(self.winning_numbers)
-        print("\n")
-        return
+                self.card_nums = card_nums
+        self.winners = list(set(self.win_nums) & set(self.card_nums))
 
 
-def calculate_copies(cards):
-    return
+def calculate_copies(cards: [Card]):
+    """Loops through each copy of each card and increments the future cards according to the card's number of winners
+
+    :param cards: Pass im a list of Card objects created by the Class in this file
+    :type cards: List of Cards
+    :return: returns an int of the number of total copies
+    :rtype: integer
+    """
+    total_copies = 0
+
+    for index, card in enumerate(cards):
+        # print("Current Card:", card)
+        copies = card.copies
+        ### Run through each copy of this card
+        while copies != 0:
+            copies -= 1
+            num_of_winners = len(card.winners)
+            ### Increment the copies of the next winners by 1
+            while num_of_winners != 0:
+                cards[index + num_of_winners].copies += 1
+                num_of_winners -= 1
+
+        ### Add the copies of this card to the total number of copies
+        total_copies += card.copies
+
+    return total_copies
 
 
 if __name__ == "__main__":
-    # test_str = "Card 123: 41 48  3 86 17 | 83 86  6 31 17  9 48 53\n"
-    # card_obj = Card(test_str)
-    # print(card_obj)
     card_list = []
-    with open("puzzle_inputs/test4.txt", "r") as file:
+    with open("puzzle_inputs/day4.txt", "r") as file:
         for line in file:
             card_list.append(Card(line))
-            # print(Card(line).winning_numbers)
-    calculate_copies(card_list)
-
-    # print("Points in cards:", calc_total_points(card_list))
+    total_copies = calculate_copies(card_list)
+    print(f"Total number of cards: {total_copies}")
